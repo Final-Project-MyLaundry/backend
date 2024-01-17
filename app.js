ef74a45dce1d92cd7c01478e34d14feb2cf64429
wahyuragil21 <why.yuragil@gmail.com>
2024-01-17 13:13:05 +0700

    Feat : add endpoint and feature register

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const UserModel = require('./db/models/userModel')
 const app = express()
 const port = 3000
 
-app.get('/user', UserModel.getUsers)
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
+
+app.get('/users', UserModel.getUsers)
+app.post('/users/register', UserModel.registerUser)
+
 
 app.get('/outlet', UserModel.getUsers)
 
--- db/models/userModel.js
+++ db/models/userModel.js
@@ -11,6 +11,36 @@ class UserModel{
         const user = await getCollection('users').find().toArray();
         await res.json(user)
     }
+
+    static async registerUser(req, res){
+
+        try {
+            const user = req.body
+        console.log(req.body)
+
+        if (!user.name) throw Error('name is required')
+
+        if (!user.email) throw Error('email is required')
+
+        if (!user.password) throw Error('password is required')
+
+        if (user.password.length < 6) throw Error('password must be at least 6 characters')
+
+        const checkUser = await getCollection('users').findOne({email : user.email})
+        if (checkUser) throw Error('account already registered');
+        
+        const result = await getCollection('users').insertOne(user)
+        await res.json({
+            message : 'success register',
+            _id : result.insertedId,
+            ...user
+            
+        }) 
+        } catch (error) {
+           res.json({message : error.message})
+        }
+       
+    }
 }
 
 module.exports = UserModel
